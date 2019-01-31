8318f09ddf9cf0a0395636eb200dcdfed4cba178
Maxim Gaidarji <maxim.gaidarji@plovdivsoftware.com>
2019-01-31 21:54:56 +0200

    Implemented logic of basic tasks.

--- Entities.js
+++ Entities.js
@@ -1,16 +1,76 @@
-class User{
-    constructor( name, age, gender ){
-        if( typeof name === 'string' )
-            console.log();
+class Customer{
+
+    constructor( name, age, gender )
+    {
         this.name   = name;
         this.age    = age;
         this.gender = gender;
+        this.guid   = generateGuid();
     }
-    validateConst( name, age, gender )
+
+    static fact( name, age, gender )
     {
-        if( ( typeof name == 'string' || name instanceof String ) )
+        if( ! Customer.validateCustomer() )
+            console.log( 'Invalid customer data' );
+        else
+            return new Customer( name, age, gender );
+    }
+
+    static validate( name, age, gender )
+    {
+        if( name instanceof Customer )
+            return true;
+        if( !( typeof name == 'string' || name instanceof String ) )
+            return false;   
+        if( name.length <= 0 || name.length > 50 )
             return false;
         if( ! Number.isInteger( age ) )
             return false;
+        if( age <= 1 || age > 120 )
+            return false;
+        if( ! typeof gender === 'boolean' )
+            return false;
+
+        return true;
     }
 }
+
+class Event{
+    constructor( title, hasFreeAccess )
+    {
+        this.title          = title;
+        this.hasFreeAccess  = hasFreeAccess;
+        this.guid           = generateGuid();   
+        this.customers      = [];
+    }
+
+    toString(){
+        var str = this.title;
+        str += this.hasFreeAccess ? ' : 18+' : '';
+        return str;
+    }
+
+    static fact( title, hasFreeAccess ){
+        if( ! Event.validate( title, hasFreeAccess ) ){
+            console.log( 'Invalid event data' );
+            return;
+        }
+        if( hasFreeAccess == undefined )
+            hasFreeAccess   = true;
+
+        if( title instanceof Event )
+            return new Event( title.title, title.hasFreeAccess )
+
+        return new Event( title, hasFreeAccess )
+    }
+
+    static validate( title, hasFreeAccess ) {
+        if( title instanceof Event )
+            return true;
+        if( !( typeof title == 'string' || title instanceof String ) )
+            return false; 
+        
+        return true;
+    }
+}
+
--- main.js
+++ main.js
@@ -1,3 +1,84 @@
-var test = () =>{
-    console.log( 'Hello world' );
+class EventMan{
+    constructor(){
+        this.events  = [];
+    }
+
+    addEvent( event )
+    {
+        if( Event.validate( event ) )
+        {
+            this.events.push(event);
+            return true;
+        }
+        return false;
+    }
+    addCustomer( evGuid, customer )
+    {
+        if( !( typeof evGuid == 'string' || evGuid instanceof String ) )
+            return false; 
+        
+        var ev = this.events.reduce( i => i.guid == evGuid );
+
+        if( ev == undefined || ! Customer.validate( customer ) )
+            return false;
+
+        if( ! ev.hasFreeAccess && customer.age < 18 )
+            return false;
+
+        ev.customers.push( customer ); 
+
+        return true;
+    }
+
+    removeCustomer( evGuid, custGuid )
+    {
+        if( !( typeof evGuid == 'string' || evGuid instanceof String ) )
+            return false; 
+    
+        if( !( typeof custGuid == 'string' || custGuid instanceof String ) )
+            return false; 
+
+        var ev = this.events.reduce( i => i.guid == evGuid );
+        ev.customers.splice( ev.customers.reduce( i => i.guid == custGuid ) );
+
+        return true;
+    }
+
+    resetUI()
+    {
+        var innerHtml = '';
+
+        this.events.forEach( element =>{
+            innerHtml += '<li><div style="display:flex; flex-direction : row" class="eventDiv" id="' 
+                      + element.guid + '"><p>' 
+                      + element.toString() + '<p><button>Delete</button></div></li>'
+        });
+
+        var arr = document.getElementsByClassName( 'eventDiv' );
+        document.getElementById( 'eventList' ).innerHTML = innerHtml;
+
+        Array.from( arr ).forEach( i => {
+            i.click( i => this.remove( i.id ) )
+        } )
+    }
+    remove( guid )
+    {
+        if( !( typeof guid == 'string' || guid instanceof String ) )
+            return false; 
+
+        this.events.splice( this.events.reduce( i => i.guid == guid ), 1 );
+    }
 }
+
+var evMan = new EventMan();
+
+evMan.add( Event.fact( 'edsasa', true ) );
+evMan.add( Event.fact( 'edfasa', false ) );
+evMan.add( Event.fact( 'edavsa', true ) );
+evMan.add( Event.fact( 'edaadsa', true ) );
+evMan.add( Event.fact( 'edaafasa', false ) );
+evMan.add( Event.fact( 'edasa', true ) );
+evMan.add( Event.fact( 'edasa', false ) );
+
+
+
